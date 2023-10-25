f61055714f06dd607be1d416a9565f504e4dcfd5
Latifah-cmd <latipharabdulnoor@gmail.com>
2023-10-25 23:02:04 +0300

    models

--- webprojects/models.py
+++ webprojects/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+class Client(models.Model):
+    #PAYMENT_OPTIONS =[
+     #   ("MM","mobilemoney")
+      #  ("VC","visacard")
+       # ("CD","cashondelievery")
+  
+    #]
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=15)
+    contact = models.CharField(max_length=15)
+    email = models.EmailField(max_length=200)
+    address = models.CharField(max_length=20)
+    product = models.CharField(max_length=30)
+    size = models.CharField(max_length=15)
+    #payment_opts = models.CharField(max_length=15, choices=PAYMENT_OPTIONS)
 
-# Create your models here.
+
+    class Order(models.Model):
+         product_name = models.CharField(max_length=30)
+         product_color = models.CharField(max_length=30)
+         order_date = models.DateTimeField(auto_now=True)
+         delievery_date = models.DateTimeField(auto_now=True)
+        # size = models.ForeignKey(Client, on_delete= models.CASCADE)
+
+
+
+    
